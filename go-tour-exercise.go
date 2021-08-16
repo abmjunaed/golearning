d816b3904e49507f3937eedb07943bea7bee3488
abmjunaed <abm.junaed@gmail.com>
2021-08-16 22:54:21 +0200

    2 solutions to map exercise

--- go-tour-exercise.go
+++ go-tour-exercise.go
@@ -28,3 +28,52 @@ func main() {
 	fmt.Println(Sqrt(4))
 	fmt.Println("Diff with lib: %g", math.Sqrt(4)-Sqrt(4))
 }
+
+
+//https://tour.golang.org/moretypes/23
+// exercise-maps.go
+//sol 1
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	m:=make(map[string]int)
+	for _,v:= range(words) {
+		elem, ok:=m[v]
+		if ok{
+			m[v]=elem+1
+		} else {
+			m[v]=1
+		}
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
+
+//Sol 2
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m:=make(map[string]int)
+	for _,v:= range(strings.Fields(s)) {// make a slice of the sentence accroding to the space
+		m[v]++
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
